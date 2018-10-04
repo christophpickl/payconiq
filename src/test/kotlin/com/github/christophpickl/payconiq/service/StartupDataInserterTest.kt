35c2a9275c50a0937d7d385df64f505321783581
christophpickl <christoph.pickl@gmail.com>
2018-10-04 19:07:27 +0200

    #8 fix import

--- src/test/kotlin/com/github/christophpickl/payconiq/service/StartupDataInserterTest.kt
+++ src/test/kotlin/com/github/christophpickl/payconiq/service/StartupDataInserterTest.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito.atLeast
 import org.mockito.Mockito.never
+import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.core.env.Environment
 
 @ExtendWith(MockitoExtension::class)
